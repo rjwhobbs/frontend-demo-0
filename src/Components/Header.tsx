ffbecd0be8e74b46f75b1f45139c6c6ec7074f4f
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2021-10-15 13:33:21 +0200

    Base layout for nav.

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,31 +1,47 @@
 import { FunctionComponent } from "react";
 import { Link } from "react-router-dom";
+import headerImg from "url:../../public/images/image-header.jpg";
+import logoWhite from "url:../../public/icons/logo-white.svg";
 
 const Header: FunctionComponent = () => {
   return (
-    <header>
-      <nav className="">
-        <ul>
-          <li>
+    <header className="relative">
+      <nav className="absolute top-0 px-10 w-full">
+        <ul className="mt-10 flex justify-end items-center">
+          <li className="mr-auto">
             <Link to="/">
-              <span aria-hidden="true">sunnyside</span>
+              <img
+                aria-hidden="true"
+                className="w-48"
+                src={logoWhite as string}
+                alt="logo"
+              />
               <span className="visually-hidden">Home</span>
             </Link>
           </li>
-          <li>
-            <Link to="#">About</Link>
+          <li className="mr-12">
+            <Link to="#" className="text-white text-lg">
+              About
+            </Link>
           </li>
-          <li>
-            <Link to="#">Services</Link>
+          <li className="mr-12">
+            <Link to="#" className="text-white text-lg">
+              Services
+            </Link>
           </li>
-          <li>
-            <Link to="#">Projects</Link>
+          <li className="mr-12">
+            <Link to="#" className="text-white text-lg">
+              Projects
+            </Link>
           </li>
-          <li>
-            <Link to="#">Contact</Link>
+          <li className="bg-white flex rounded-full">
+            <Link to="#" className="font-serif uppercase text-lg py-4 px-7">
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
+      <img className="" src={headerImg as string} alt="sliced orange" />
     </header>
   );
 };
