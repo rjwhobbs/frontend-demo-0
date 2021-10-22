f77596281884bc8961daa5a0face8b76e6ac8a14
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2021-10-22 17:39:32 +0200

    Making an underline.

--- src/Components/LandingComps/BrandDescription.tsx
+++ src/Components/LandingComps/BrandDescription.tsx
@@ -7,14 +7,18 @@ const BrandDescription: FunctionComponent<DesProps> = (props) => {
   const marginAlign = margin ? margin + "-3" : "";
   return (
     <div className="flex items-center justify-center">
-      <div className={marginAlign + " h-2/3 w-2/3"}>
+      <div className={marginAlign + " h-2/3 w-2/3 relative"}>
         <h2 className="font-serif text-desat-blue text-4.5xl leading-tight">
           {heading}
         </h2>
         <p className="text-grey-blue text-lg my-10">{content}</p>
-        <Link to="#" className="uppercase font-serif">
+        <Link
+          to="#"
+          className="relative z-10 learn-more text-lg uppercase font-serif"
+        >
           Learn more
         </Link>
+        <div className="learn-more-line"></div>
       </div>
     </div>
   );
